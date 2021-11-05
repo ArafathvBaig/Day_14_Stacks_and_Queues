a690960bb237a16e98f54e4d2800ce518af1c4e8
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-11-05 18:29:12 +0530

    Updated: Java Program With UC3

--- src/com/Day_14_Stacks_and_Queues/Stacks_and_Queues.java
+++ src/com/Day_14_Stacks_and_Queues/Stacks_and_Queues.java
@@ -3,7 +3,9 @@ package com.Day_14_Stacks_and_Queues;
 public class Stacks_and_Queues 
 {
    public Node head; // the first node
-
+   public Node tail; // the last node
+   int currentSize;
+   
    // nest class to define node
    public class Node 
    {
@@ -14,6 +16,8 @@ public class Stacks_and_Queues
    public Stacks_and_Queues()
    {
        head = null;
+       tail = null;
+       currentSize = 0;
    }
 
    //Remove all the elements, till the stack is empty
@@ -32,7 +36,7 @@ public class Stacks_and_Queues
 	   }
    }
 
-   // Add value to the beginning of the list for demonstrating behaviour of stack
+   // Add value to the beginning of the list to demonstrate the behavior of stack
    public void push(int value) 
    {
        Node oldHead = head;
@@ -40,6 +44,26 @@ public class Stacks_and_Queues
        head.value = value;
        head.next = oldHead;
    }
+   
+   // Add values to the end of the list, so that the first element added will stay at Top
+   public void enqueue(int value) 
+   {
+		Node oldHead = head;
+		head = new Node();
+		head.value = value;
+		head.next = null;
+		if (currentSize==0) 
+		{
+			tail = head;
+		}
+		else 
+		{
+			oldHead.next = head;
+		}
+		currentSize++;
+		System.out.println(value + " added to the queue");
+	}
+   
    public static void printList(Node head) 
    {
        Node temp = head;
@@ -57,11 +81,14 @@ public class Stacks_and_Queues
        saq.push(70);
        saq.push(30);
        saq.push(56);
-       System.out.println("Elements Before POP");
+       System.out.println("Elements After PUSH");
        printList(saq.head);
        System.out.println("Elements After POP");
        saq.pop();
        printList(saq.head);
+       saq.enqueue(56);
+       saq.enqueue(30);
+       saq.enqueue(70);
    }
 }
 
